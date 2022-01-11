839c8f6a04bdd18965483680f76ac7ceaae34e31
RosalieChhen <mei-heang.chhen@heig-vd.ch>
2022-01-11 09:11:14 +0100

    initial commit

---
+++ app/src/main/java/ch/heivd/iict/sym/labo4/MainActivity.kt
@@ -0,0 +1,19 @@
+package ch.heivd.iict.sym.labo4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var compass_btn: Button
+    private lateinit var BLE_btn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        compass_btn = findViewById(R.id.main_compass_btn)
+        BLE_btn = findViewById(R.id.main_BLE_btn)
+    }
+}
